e031bfa24ff5caea9f3aa623f02214f6e0e68497
Miroslaw Kapinos <mkapinos@agileo.pl>
2019-08-01 13:14:20 +0200

    Freezer test

---
+++ projects/freezer/src/lib/freezer.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { FreezerComponent } from './freezer.component';
+
+@NgModule({
+  declarations: [FreezerComponent],
+  imports: [
+  ],
+  exports: [FreezerComponent]
+})
+export class FreezerModule { }
