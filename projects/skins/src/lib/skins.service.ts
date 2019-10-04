706bfc261b1906648348f7726241e0306217f9ed
miro <mkapinos@agileo.pl>
2019-10-04 09:11:45 +0200

    Refactoring to es6

--- projects/skins/src/lib/skins.service.ts
+++ projects/skins/src/lib/skins.service.ts
@@ -1,32 +1,34 @@
 import { Inject, Injectable } from '@angular/core';
 import { FLEXY_SKINS_LIST_TOKEN } from './skins.provider-token';
 
+const SKIN_STATE_WINDOW_NAME = 'skinState';
+
 @Injectable()
 export class FlexySkinsService {
   private readonly supported: string[];
 
   // its global class skin-switcher.js
-  private _skinStateService: {
+  private skinStateService: {
     getDefault: () => string;
     switch: (skin: string) => boolean;
-  } = window['skinState'];
+  } = window[SKIN_STATE_WINDOW_NAME];
 
   constructor(@Inject(FLEXY_SKINS_LIST_TOKEN) supportedSkins) {
     this.supported = supportedSkins;
     console.log('supported skins', this.supported);
-    const currentSkin = this._skinStateService.getDefault();
+    const currentSkin = this.skinStateService.getDefault();
     if (!this.supported.includes(currentSkin)) {
-      this._skinStateService.switch(supportedSkins[0]);
+      this.skinStateService.switch(supportedSkins[0]);
     }
   }
 
   getCurrent(): string {
-    return this._skinStateService.getDefault();
+    return this.skinStateService.getDefault();
   }
 
   set(skin: string): boolean {
     if (this.supported.includes(skin)) {
-      this._skinStateService.switch(skin);
+      this.skinStateService.switch(skin);
       return true;
     } else {
       console.warn('Skin is not supported', skin);
--- projects/skins/src/scripts/skin-switcher.js
+++ projects/skins/src/scripts/skin-switcher.js
@@ -1,13 +1,30 @@
-var STYLE_SKIN_LOCAL_STORAGE_ID = 'SKIN_NAME';
-var STYLE_SKIN_DEFAULT = 'default';
+const STYLE_SKIN_LOCAL_STORAGE_ID = 'SKIN_NAME';
+const STYLE_SKIN_DEFAULT = 'default';
 
-var skinState = new (function() {
+// http://stackoverflow.com/a/26227662/1527470
+const singleton = Symbol();
+const singletonEnforcer = Symbol();
 
-  this.findLink = function(skin) {
-    var links = document.querySelectorAll('link[data-skin]');
-    var skinLink;
-    for (var i = 0; i < links.length; i++) {
-      var link = links[i];
+class FlexySkinState {
+  constructor(enforcer) {
+    if (enforcer != singletonEnforcer) {
+      throw 'Cannot construct singleton';
+    }
+    this.switch(this.getDefault());
+  }
+
+  static getInstance() {
+    if (!this[singleton]) {
+      this[singleton] = new FlexySkinState(singletonEnforcer);
+    }
+    return this[singleton];
+  }
+
+  findLink(skin) {
+    const links = document.querySelectorAll('link[data-skin]');
+    let skinLink;
+    for (let i = 0; i < links.length; i++) {
+      let link = links[i];
       if (link.dataset && link.dataset.skin) {
         if (link.dataset.skin === skin) {
           skinLink = link;
@@ -15,11 +32,11 @@ var skinState = new (function() {
       }
     }
     return skinLink;
-  };
+  }
 
-  this.isLoaded = function(skinLink) {
+  isLoaded(skinLink) {
     if (skinLink) {
-      var styleSheet = [].slice.call(document.styleSheets).find(function (item) {
+      const styleSheet = [].slice.call(document.styleSheets).find(item => {
         return item.href && item.href.indexOf(skinLink.getAttribute('href')) !== -1;
       });
       return !!styleSheet;
@@ -27,40 +44,41 @@ var skinState = new (function() {
     return false;
   }
 
-  this.disableSkins = function(skin) {
-    var links = document.querySelectorAll('link[data-skin]');
-    for (var i = 0; i < links.length; i++) {
-      var link = links[i];
+  disableSkins(skin) {
+    const links = document.querySelectorAll('link[data-skin]');
+    links.forEach(link => {
       if (link.dataset && link.dataset.skin) {
         if (link.dataset.skin !== skin) {
           link.disabled = true;
         }
       }
-    }
-  };
+    });
+  }
 
-  this.switch = function(skin) {
-    var skinLink = this.findLink(skin);
-    if (!this.isLoaded(skinLink)) {
-      skinLink.onload = function () {
+  switch(skin) {
+    const skinLink = this.findLink(skin);
+    if (skinLink) {
+      if (!this.isLoaded(skinLink)) {
+        skinLink.onload = () => {
+          this.disableSkins(skin);
+        };
+      } else {
         this.disableSkins(skin);
-      }.bind(this);
-    } else {
-      this.disableSkins(skin);
+      }
+      skinLink.disabled = false;
+      this.setDefault(skin);
     }
-    skinLink.disabled = false
-    this.setDefault(skin);
-  };
+  }
 
-  this.getDefault = function() {
-    var skin = localStorage.getItem(STYLE_SKIN_LOCAL_STORAGE_ID);
+  getDefault() {
+    const skin = localStorage.getItem(STYLE_SKIN_LOCAL_STORAGE_ID);
     return skin ? skin : STYLE_SKIN_DEFAULT;
-  };
+  }
 
-  this.setDefault = function(skin) {
+  setDefault(skin) {
     localStorage.setItem(STYLE_SKIN_LOCAL_STORAGE_ID, skin);
-  };
+  }
+}
 
-  this.switch(this.getDefault());
-  console.log('Skin', this.getDefault());
-})();
+// Init skin state
+window['skinState'] = FlexySkinState.getInstance();
