e99088b1ae8106c0df7655d66e188563e2ec7abc
“mkapinos” <miroslaw@cledar.pl>
2019-10-28 08:07:03 +0100

    Layout doc

---
+++ projects/demo/src/app/layout/layout.module.ts
@@ -0,0 +1,34 @@
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TabsModule } from 'ngx-bootstrap';
+import { AppCommonModule } from '../common/common.module';
+import { FlexyLoggerModule } from '@ng-flexy/core';
+import { DemoLayoutDocInfoComponent } from './components/layout-doc-info.component';
+import { DemoLayoutDocComponent } from './components/layout-doc.component';
+import { FlexyLayoutModule } from '@ng-flexy/layout';
+import { DemoLayoutDocJsonComponent } from './components/layout-doc-json.component';
+import { DemoLayoutJsonComponent } from './components/layout-json.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DemoLayoutDocComponent,
+    children: [
+      {
+        path: '',
+        component: DemoLayoutDocInfoComponent
+      },
+      {
+        path: 'json',
+        component: DemoLayoutDocJsonComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [CommonModule, AppCommonModule, RouterModule.forChild(routes), TabsModule, FlexyLoggerModule, FlexyLayoutModule.forChild({})],
+  declarations: [DemoLayoutDocComponent, DemoLayoutDocInfoComponent, DemoLayoutDocJsonComponent, DemoLayoutJsonComponent]
+})
+export class DemoLayoutModule {}
